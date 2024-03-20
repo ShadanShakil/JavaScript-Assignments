57770a27af3450c9ec959e72d12300a03cc0336c
Khawaja Shadan Shakil <shadanshakil9@gmail.com>
2024-03-20 22:25:01 +0500

    for loop added

---
+++ for loops/index.js
@@ -0,0 +1,20 @@
+// question 1
+
+for(i=0; i <= 10; i++){
+    console.log(i);
+}
+
+// question 2
+
+for(i=1; i <=10; i++){
+    console.log(i * 2);
+}
+
+// question 3
+
+for(i=1; i <= 15 ; i+=2){
+    console.log(i);
+}
+
+// question 4
+
